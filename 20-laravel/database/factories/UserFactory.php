2c44b6b44ac51fa52aebba8c96f814d68ed0e223
lf_cardona-admin <luisfer.cardona13@gmail.com>
2025-10-30 11:39:47 -0500

    vistas

--- 20-laravel/database/factories/UserFactory.php
+++ 20-laravel/database/factories/UserFactory.php
@@ -24,11 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'document'  => fake()->numerify('75#######'),
+            'fullname'  => fake()->firstName().' '.fake()->lastName(),
+            'gender'    => fake()->randomElement(['Female','Male']),
+            'birthdate' => fake()->date(),
+            'phone'     => fake()->numerify('310######'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(10)
+
         ];
     }
 
--- 20-laravel/database/seeders/DatabaseSeeder.php
+++ 20-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\pet;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,7 +22,11 @@ class DatabaseSeeder extends Seeder
             PetSeeder::class,
             AdoptionSeeder::class,
         ]);
-        // User::factory(10)->create();
+        User::factory(100)->create();
+
+        pet::factory(40)->create();
+        
+
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',

e0d2f20e321634ad40eb8bf6beb135823628f869
lf_cardona-admin <luisfer.cardona13@gmail.com>
2025-11-11 17:37:48 -0500

    home and login

--- 20-laravel/routes/web.php
+++ 20-laravel/routes/web.php
@@ -1,67 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return "this is a entry point: 👌";
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('hello', function(){
-    return "<h1>hello folks, have a nice day ✅  </h1>";
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('hello/{name}', function(){
-    return "<h1>hello: ".request()->name."</h1>";
-});
-
-Route::get( 'show/pets',function(){
-    $pets = App\Models\Pet::all();
-    dd($pets->toArray());
-});
-
-Route::get( 'show/pets/{id}',function($id){
-    $pets = App\Models\Pet::find($id);
-    dd($pets->toArray());
-});
-
-Route::get('challenge', function() {
-    $users = App\Models\User::take(20)->get();
-    
-    $html = '<table border="1" style="border-collapse: collapse; width: 100%;">
-        <thead>
-            <tr style="background: #f4f4f4;">
-                <th>ID</th>
-                <th>Name</th>
-                <th>Photo</th>
-                <th>Age</th>
-                <th>Created At</th>
-            </tr>
-        </thead>
-        <tbody>';
-    
-    foreach($users as $user) {
-        $age = \Carbon\Carbon::parse($user->birthdate)->age;
-        $html .= "<tr>
-            <td>{$user->id}</td>
-            <td>{$user->fullname}</td>
-            <td><img src='{$user->photo}' width='50' height='50'></td>
-            <td>{$age}</td>
-            <td>{$user->created_at->format('Y-m-d')}</td>
-        </tr>";
-    }
-    
-    $html .= '</tbody></table>';
-    
-    return $html;
-});
-
-//
-Route::get('view/pets', function () {
-    $pets = \App\Models\Pet::all();
-    return view('view-pets')->with('pets', $pets);
-});
-
-
+require __DIR__.'/auth.php';
